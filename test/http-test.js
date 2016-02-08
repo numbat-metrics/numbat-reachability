fa90a19889009fe5fb9965a2e39fcd3c931e2b16
Ryan Day <soldair@gmail.com>
2016-02-08 07:21:22 -0800

    adding unreachable test for http

--- test/http-test.js
+++ test/http-test.js
@@ -41,3 +41,32 @@ test("can test http",function(t){
     },100)
   }).listen(TARGET_PORT)
 })
+
+
+
+test('unreachable',function(t){
+
+    var collector = numock(ANALYZER_PORT)
+
+    stopProducer = Producer({
+      uri: 'tcp://127.0.0.1:' + ANALYZER_PORT,
+      hosts: [
+        'http://127.0.0.1:' + TARGET_PORT
+      ],
+      interval: 100
+    })
+
+
+    collector.once('data',function(){
+      stopProducer()
+      collector.finished(function(err,metrics){
+
+        console.log(metrics)
+        t.equals(metrics[0].name,'reachability','should have correct name')
+        t.equals(metrics[0].value,0,'should not be reachable')
+
+        t.end()
+      })
+    })
+
+})
