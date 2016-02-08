577d1d37f8d2882d155c3b99de28bf73dde2941b
Ryan Day <soldair@gmail.com>
2016-02-08 06:32:42 -0800

    adding unreachable test

--- test/net-test.js
+++ test/net-test.js
@@ -34,3 +34,31 @@ test("can test tcp",function(t){
   })
 
 })
+
+
+test('unreachable',function(t){
+
+    var collector = numock(ANALYZER_PORT)
+
+    stopProducer = Producer({
+      uri: 'tcp://127.0.0.1:' + ANALYZER_PORT,
+      hosts: [
+        'tcp://127.0.0.1:' + TARGET_PORT
+      ],
+      interval: 100
+    })
+
+
+    collector.once('data',function(){
+      stopProducer()
+      collector.finished(function(err,metrics){
+
+        console.log(metrics)
+        t.equals(metrics[0].name,'reachability','should have correct name')
+        t.equals(metrics[0].value,0,'should not be reachable')
+
+        t.end()
+      })
+    })
+
+})
